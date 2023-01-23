af0783ee074d06a6e71152e2a89187977c87f78f
jibdrt <jb.druet@codeur.online>
2023-01-23 22:11:15 +0100

    edit route

--- src/components/UpdateUser.js
+++ src/components/UpdateUser.js
@@ -1,6 +1,13 @@
 
-export default function UpdateUser() {
-    return (
-        <h1>Update User</h1>
-    );
+
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import { useNavigate, useParams } from "react-router-dom";
+export default function ListUser() {
+    const navigate = useNavigate();
+    const [inputs, setInputs] = useState([]);
+    const {id} = useParams();
+    useEffect(() => {
+        getUser();
+    }, []);
 }
